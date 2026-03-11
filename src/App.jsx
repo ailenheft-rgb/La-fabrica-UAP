1c4776f3511a1f6ef2b792fa04f9d5aca501c60d
ailenheft-rgb <ailen.heft@gmail.com>
2026-03-10 23:42:43 -0300

    cambio de correo de administrador

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const firebaseConfig = {
   measurementId: "G-V4N3P2QT3M"
 };
 
-const ADMIN_EMAIL = "ailen.heft@gmail.com"; 
+const ADMIN_EMAIL = "nairmartinez2001@gmail.com"; 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
